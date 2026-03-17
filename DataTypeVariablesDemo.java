90818b9fd32d6cd2dbd9286b040535d8e04a136f
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-03-17 13:36:49 +0530

    datatypes and variables demo

---
+++ DataTypeVariablesDemo.java
@@ -0,0 +1,17 @@
+public class DataTypeVariablesDemo{
+	public static void main(String[] args){
+	  int age=30;
+	  String name="malli";
+	  long phone=7984465784l;
+          double balance=28.4442;
+	  float temp=97.5f;
+          boolean isActive=false;
+          char gender='m';
+          System.out.println("your age is : "+age);
+          System.out.println("your name is : "+name);
+          System.out.println("your PhoneNumber is : "+phone);
+          System.out.println("your balance is : "+balance);
+          System.out.println("your account is active? : "+isActive);
+          System.out.println("Gender is : "+gender);
+	}
+}
