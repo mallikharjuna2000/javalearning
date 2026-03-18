d12116d159c0446f13078178571fbdc14f7abac8
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-03-18 12:38:11 +0530

    datatype casting implicit and explicit

---
+++ TypeCastingDemo.java
@@ -0,0 +1,11 @@
+public class TypeCastingDemo{
+	public static void main(String[] args){
+          int x=10;
+          int y=x++;
+         System.out.println(x);//
+        System.out.println(y);//
+	  	}
+}
+
+
+
