ae7b0b6d078f44e7e5c1d74cdf34ead367c63bb2
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-03-25 13:00:26 +0530

    sum of digits in a given number

---
+++ src/com/codegnan/SumOfDigits.java
@@ -0,0 +1,23 @@
+package com.codegnan;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		int number = scanner.nextInt();//123
+		int originalNumber = number;//123
+		int digitSum = 0;
+		while (number != 0) {//1>0
+			int lastDigit = number % 10;//1%10->1
+			digitSum += lastDigit;//0+3==>3+2-->5+1-->6
+			number /= 10;//123/10-->12/10-->1/10-->012
+		}
+		System.out.println("The sum of digits in a Given Number " + originalNumber + " is =" + digitSum);
+		scanner.close();
+
+	}
+
+}
