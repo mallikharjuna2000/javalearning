40ba1fe13a144e84acc9dcbbba009c274cf3fcab
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-04-25 13:42:37 +0530

    feat: thread yeild stop thread exceution example

--- src/com/codegnan/threads/MyRunnable.java
+++ src/com/codegnan/threads/MyRunnable.java
@@ -4,7 +4,10 @@ public class MyRunnable implements Runnable {
 
 	@Override
 	public void run() {
-	
+		for (int i = 0; i < 10; i++) {
+			Thread.yield();
+			System.out.println("Child Thread");
+		}
 	}
 
 }
---
+++ src/com/codegnan/threads/ThreadYeildDemo.java
@@ -0,0 +1,15 @@
+package com.codegnan.threads;
+
+public class ThreadYeildDemo {
+
+	public static void main(String[] args) {
+		MyRunnable r = new MyRunnable();
+		Thread t = new Thread(r);
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Main Thread");
+		}
+
+	}
+
+}
