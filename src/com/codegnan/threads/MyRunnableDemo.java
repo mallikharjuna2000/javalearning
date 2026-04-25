3615a65a68f58544535852468918b92333d3c2b6
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-04-25 12:46:48 +0530

    feat: implementing Runnable interface approach

---
+++ src/com/codegnan/threads/MyRunnableDemo.java
@@ -0,0 +1,14 @@
+package com.codegnan.threads;
+
+public class MyRunnableDemo {
+
+	public static void main(String[] args) {
+		MyRunnable r = new MyRunnable();
+		Thread t=new Thread(r);
+		t.start();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Main class Thread");
+		}
+	}
+
+}
