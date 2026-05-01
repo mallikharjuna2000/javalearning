7aa55977dccec016d705bae1ea7280654e42e2c8
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-05-01 15:25:45 +0530

    feat: transient Modifier in Serialization

--- src/com/codegnan/java8/SerDesrDemo.java
+++ src/com/codegnan/java8/SerDesrDemo.java
@@ -1,28 +1,30 @@
 package com.codegnan.java8;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class SerDesrDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Serialization started");
-		String filepath="emp.ser";
-		FileOutputStream fos=null;
-		ObjectOutputStream oos=null;
+		String filepath = "emp.ser";
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
 		try {
-			 fos=new FileOutputStream(filepath);
-			oos=new ObjectOutputStream(fos);
-			Student s=new Student(101,"malli","malli@codegnan");
+			fos = new FileOutputStream(filepath);
+			oos = new ObjectOutputStream(fos);
+			Student s = new Student(101, "malli", "malli@codegnan","malli123");
 			System.out.println("Before serialization student details ");
 			s.displayStudentDetails();
 			oos.writeObject(s);
-			System.out.println("Serailized Student Object : "+s);
+			System.out.println("Serailized Student Object : " + s);
 			System.out.println("Serialization Endede");
-		}catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
 				oos.close();
 				fos.close();
@@ -31,7 +33,16 @@ public class SerDesrDemo {
 				e.printStackTrace();
 			}
 		}
-
+		System.out.println("De-Serialization started");
+		try (FileInputStream fis = new FileInputStream(filepath); 
+				ObjectInputStream ois = new ObjectInputStream(fis)) {
+			Student s2 = (Student) ois.readObject();
+			System.out.println("Student details after Deserailization ");
+			s2.displayStudentDetails();
+			System.out.println("De-Serialized Object : " + s2);
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
